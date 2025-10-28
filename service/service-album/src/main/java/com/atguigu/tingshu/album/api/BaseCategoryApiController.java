2dc554a46f9524a3289a1558ef6d54a5d3112cfe
yanjiazheng <1453562554@qq.com>
2025-10-28 14:56:06 +0800

    feat(album): 实现分类数据查询接口- 新增分类管理API控制器BaseCategoryApiController - 添加查询所有分类数据的REST接口getBaseCategoryList - 实现分类数据按层级结构组装逻辑 - 集成Swagger注解优化接口文档 - 在BaseCategoryService中定义分类查询方法- 通过BaseCategoryViewMapper查询完整分类视图数据 - 使用Stream API对分类数据进行分组和映射处理- 返回封装后的多级分类JSON结构数据

--- service/service-album/src/main/java/com/atguigu/tingshu/album/api/BaseCategoryApiController.java
+++ service/service-album/src/main/java/com/atguigu/tingshu/album/api/BaseCategoryApiController.java
@@ -1,20 +1,40 @@
 package com.atguigu.tingshu.album.api;
 
+import com.alibaba.fastjson.JSONObject;
 import com.atguigu.tingshu.album.service.BaseCategoryService;
+import com.atguigu.tingshu.common.result.Result;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @Tag(name = "分类管理")
 @RestController
-@RequestMapping(value="/api/album/category")
+@RequestMapping(value = "/api/album/category")
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class BaseCategoryApiController {
-	
-	@Autowired
-	private BaseCategoryService baseCategoryService;
+
+    @Autowired
+    private BaseCategoryService baseCategoryService;
+
+    /**
+     * 查询所有分类数据
+     *
+     * @return
+     */
+    @Operation(tags = "查询所有分类数据")
+    @GetMapping("getBaseCategoryList")
+    public Result getBaseCategoryList() {
+        //	调用服务层的查询分类方法
+        List<JSONObject> categoryList = this.baseCategoryService.getBaseCategoryList();
+        //	将数据返回给页面使用
+        return Result.ok(categoryList);
+    }
 
 }
 
