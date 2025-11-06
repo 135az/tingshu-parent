04d06368c18e6d36007f3e95bc9d7d8199b1e114
yanjiazheng <1453562554@qq.com>
2025-11-06 16:06:57 +0800

    feat(account): 实现用户账户余额查询功能
    - 新增获取账户可用余额的API接口
    - 实现根据用户ID查询账户余额的服务逻辑
    - 添加用户账户信息的数据访问方法
    - 集成认证上下文获取当前用户ID
    - 使用BigDecimal类型处理金额精度问题
    - 配置接口Swagger文档注解说明
    - 添加GuiGuLogin注解实现接口权限校验

--- service/service-account/src/main/java/com/atguigu/tingshu/account/service/UserAccountService.java
+++ service/service-account/src/main/java/com/atguigu/tingshu/account/service/UserAccountService.java
@@ -3,12 +3,30 @@ package com.atguigu.tingshu.account.service;
 import com.atguigu.tingshu.model.account.UserAccount;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.math.BigDecimal;
+
 public interface UserAccountService extends IService<UserAccount> {
-    
+
     /**
      * 添加用户账户
      *
      * @param userId
      */
     void addUserAccount(Long userId);
+
+    /**
+     * 获取用户账户可用余额
+     *
+     * @param userId
+     * @return
+     */
+    BigDecimal getAvailableAmount(Long userId);
+
+    /**
+     * 根据用户Id 获取到可用余额对象
+     *
+     * @param userId
+     * @return
+     */
+    UserAccount getUserAccountByUserId(Long userId);
 }
--- service/service-account/src/main/java/com/atguigu/tingshu/account/service/impl/UserAccountServiceImpl.java
+++ service/service-account/src/main/java/com/atguigu/tingshu/account/service/impl/UserAccountServiceImpl.java
@@ -3,10 +3,13 @@ package com.atguigu.tingshu.account.service.impl;
 import com.atguigu.tingshu.account.mapper.UserAccountMapper;
 import com.atguigu.tingshu.account.service.UserAccountService;
 import com.atguigu.tingshu.model.account.UserAccount;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Slf4j
 @Service
@@ -26,5 +29,17 @@ public class UserAccountServiceImpl extends ServiceImpl<UserAccountMapper, UserA
         UserAccount userAccount = new UserAccount();
         userAccount.setUserId(userId);
         userAccountMapper.insert(userAccount);
+    }
+
+    @Override
+    public BigDecimal getAvailableAmount(Long userId) {
+        //	根据用户Id 获取到用户余额对象
+        UserAccount userAccount = this.getUserAccountByUserId(userId);
+        return userAccount.getAvailableAmount();
+    }
+
+    @Override
+    public UserAccount getUserAccountByUserId(Long userId) {
+        return this.getOne(new LambdaQueryWrapper<UserAccount>().eq(UserAccount::getUserId, userId));
     }
 }
